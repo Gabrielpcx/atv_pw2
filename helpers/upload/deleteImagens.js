7d5a7217b4f69c1b5af57e1627a8bf8ef3dc6675
Gabriel <109095921+Gabrielpcx@users.noreply.github.com>
2023-12-05 20:31:47 -0300

    Update deleteImagens.js

--- helpers/upload/deleteImagens.js
+++ helpers/upload/deleteImagens.js
@@ -3,13 +3,7 @@ const { getStorage, ref, deleteObject} = require('firebase/storage');
 
 /* DADOS DE ACESSO AO FIREBASE */
 const firebaseConfig = {
-    apiKey: "AIzaSyCX2f8n-KLtwFIkUI6mHthSSaIV4Dn6KSI",
-    authDomain: "projeto-661c3.firebaseapp.com",
-    projectId: "projeto-661c3",
-    storageBucket: "projeto-661c3.appspot.com",
-    messagingSenderId: "76239141612",
-    appId: "1:76239141612:web:f3824ea55e388fd1b9c3c9",
-    measurementId: "G-QPJTYZB6V6"
+   
 };
 
 /* INICIALIZAÇÃO DO FIREBASE */
